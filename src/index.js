2dbaccae4b223b12f40c087acb7fd827b273dd9c
Martin Bliznachki <Martin_Bliznachki@epam.com>
2016-07-06 17:53:35 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import angular from 'angular';
+import ngResource from 'angular-resource';
+import '../node_modules/@angular/router/angular1/angular_1_router.js';
 import '../src/services/rest-service.js';
 import '../src/app.js';
 import '../src/components';
